663537698e32c59b73d2ceb2fa9603fed81723ef
Mathew Hartley <me@mathewhartley.com>
2014-10-23 14:11:58 +1300

    Add in product builder endpoint

--- lib/milkbottle/client/libraries.rb
+++ lib/milkbottle/client/libraries.rb
@@ -1,12 +1,16 @@
 module Milkbottle
   class Client
     module Libraries
+      def create_library_pull(url, options = {})
+        post "/library/pull", options.merge({Url: url})
+      end
+
       def create_library_multi_pull(urls, options = {})
         post "/library/multi/pull", options.merge({Urls: [*urls]})
       end
 
-      def create_library_pull(url, options = {})
-        post "/library/pull", options.merge({Url: url})
+      def create_product_builder(library_ids, options ={})
+        post "/library/builder", otions.merge({Guids: library_ids})
       end
     end
   end
